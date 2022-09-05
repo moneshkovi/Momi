2a67be894e413d2cb3cdb6e1f286281affaa44a5
Tron <67748834+moneshkovi@users.noreply.github.com>
2022-09-05 07:10:09 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+import cx_Freeze
+from cx_Freeze import *
+
+setup(
+    name = "jarvis",
+    options = ("build_exe":f('packages':['pyttsx3','speech_recognition','selenium','pyautogui','winsound','pyowm']))
+    executables={
+          Executables(
+              "jarvis.py",
+ 
+
+              )
+
+        }
+
+    )
