790ebef9004d1fa053304da9052e00b5b1901739
Tron <67748834+moneshkovi@users.noreply.github.com>
2023-01-17 12:26:15 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -7,7 +7,7 @@ root = tk.Tk()
 apps=[]
 
 if os.path.isfile('save.txt'):
-    with open('save.txt','r') as f:
+    with open('save.txt','r') as f:     
         tempApps = f.read() 
         tempApps = tempApps.split(',')
         print(tempApps)
